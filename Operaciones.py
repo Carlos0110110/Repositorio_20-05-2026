20c2b36a2de6b8e875837ccb3238015a5920abfb
Carlos0110110 <carlosmamanim04@gmail.com>
2026-05-20 16:57:35 -0400

    Rep Operaciones

---
+++ Operaciones.py
@@ -0,0 +1,14 @@
+#La suma de dos numeros
+print ("OPERACIONES")
+a = float(input("Ingrese el primer numero:   "))
+b = float(input("Ingrese el segundo numero:   "))
+
+
+print ("La suma es :  ",  a + b)
+print ("La resta es :  ", a - b)
+print ("La multi es :  ", a * b)
+if b == 0:
+    print ("Dato invalido")
+    
+else:    
+    print ("La div es :   ",   a / b)
